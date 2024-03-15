5bc2d83f4111d0b4800e56a54b241fb10a1c6390
zelayab <bernardo_zelaya@hotmail.com>
2024-03-14 21:10:19 -0300

    Add skeleton components for card and image

---
+++ src/components/skeleton/skeletonImage.tsx
@@ -0,0 +1,8 @@
+export function SkeletonImage() {
+  return (
+    <div className="flex flex-col justify-center  gap-4  animate-pulse w-full h-full lg:w-full  bg-gray-900 rounded-lg">
+      <div className="w-full h-[150px] bg-gray-900 bg-opacity-50 rounded-lg" />
+      <div className="flex flex-col items-center gap-4 p-4"></div>
+    </div>
+  );
+}
